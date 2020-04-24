dd728fea272f4d15c377522715feb31efc63ef86
Sinclair Kinch <sinclair.kinch@us.sogeti.com>
2020-04-23 23:49:15 -0400

    Updates

---
+++ precache-manifest.4a3a98c75a2b7b1f259e611ff7317b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64abd18174621a582802b2c607ce8ef9",
+    "url": "/gregslist/index.html"
+  },
+  {
+    "revision": "0cb9f501e6ed0f8bf136",
+    "url": "/gregslist/static/css/main.3cd225c7.chunk.css"
+  },
+  {
+    "revision": "7c8f2fed36bad81140ef",
+    "url": "/gregslist/static/js/2.92c2fe99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gregslist/static/js/2.92c2fe99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb9f501e6ed0f8bf136",
+    "url": "/gregslist/static/js/main.9c9a7c1e.chunk.js"
+  },
+  {
+    "revision": "0712a205e643878b923e",
+    "url": "/gregslist/static/js/runtime-main.4e3e49d2.js"
+  }
+]);
